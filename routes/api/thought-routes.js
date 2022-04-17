822640c05227157121d8f106e2726eeb9067e41c
Luke G <lukejgranered@gmail.com>
2022-04-17 09:35:30 -0500

    controllers and routes added

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    updateThought,
+    removeThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
+router.route('/').get(getAllThoughts);
+
+router.route('/:userId').post(addThought);
+
+router.route('/:userId/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .post(addReaction)
+    .delete(removeThought);
+
+router.route('/:userId/:thoughtId/:reactionId').delete(removeReaction);
+
+module.exports = router;
